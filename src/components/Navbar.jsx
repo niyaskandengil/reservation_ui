bc45767cdd67291d0272aa0e05b805abebab282d
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-12 13:34:06 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import InfoIcon from '@mui/icons-material/Info';
+import ContactSupportIcon from '@mui/icons-material/ContactSupport';
+import '../style/Navbar.css'
+
 const Navbar = () => {
     return (
         <div className='Navbar'>
@@ -7,8 +11,8 @@ const Navbar = () => {
                 <h1>Hello<span>Travels</span></h1>
             </div>
             <div className="options">
-                <Link to='/aboutus'>About Us</Link>
-                <Link to='/contactus'>Contact Us</Link>
+                <Link to='/aboutus'><InfoIcon id='mui1'/>About Us</Link>
+                <Link to='/contactus'><ContactSupportIcon/>Contact Us</Link>
                 <Link to='/userlogin'>Login</Link>
             </div>
         </div>
