33b80ba40896d945e0a0baa1a49f5d14daca4563
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-13 12:56:43 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
 import Navbar from './components/Navbar';
 import UserLogin from './components/UserLogin';
 import AboutUS from './components/AboutUS';
 import ContactUs from './components/ContactUs';
+import Homepage from './components/Homepage';
+import UserSignUp from './components/UserSignUp';
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
       <BrowserRouter>
       <Navbar />
       <Routes>
+      <Route path="/" element={<Homepage />} />
         <Route path="/userlogin" element={<UserLogin />} />
         <Route path="/aboutus" element={<AboutUS />} />
         <Route path="/contactus" element={<ContactUs />} />
+        <Route path="/usersignup" element={<UserSignUp />} />
       </Routes>
       </BrowserRouter>
     </div>
