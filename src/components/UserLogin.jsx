892f961d76c6495f2a8094c4c44ca31fecf7e6a7
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-13 12:57:08 +0530

    #

--- src/components/UserLogin.jsx
+++ src/components/UserLogin.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import '../styles/userlogin.css'
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
+    <div className='userlogin'>
+      <form action="">
+        <input type="email"  placeholder='Enter the Email'/>
+        <br /><br />
+        <input type="password"  placeholder='Enter the Password'/>
+        <br /><br />
+        <button className='button'>Login</button>
+        <br />
+        <p>New User? Click here to <Link to='/usersignup'>Register</Link></p>
+      </form>
+    </div>
   )
 }
 
