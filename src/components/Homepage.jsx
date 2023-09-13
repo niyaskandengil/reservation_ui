a9e858b0f9331f4e31cb70e5c3fac0502f0af3db
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-13 12:56:49 +0530

    #

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import '../styles/homepage.css'
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <div className='partners'>
+      <h2>Book SRTC (State Road Transport Corporation) bus tickets at AbhiBus</h2>
+      <div className='images'>
+        <img src="https://st.redbus.in/buslogos/country/ind/logo_group/10283.png"/>
+        <img src="https://s3.rdbuz.com/web/images/homeV2/rtc/GSRTC.svg" />
+        <img src="https://st.redbus.in/Images/carousel/TSRTC.png" />
+        <img src="https://s3.rdbuz.com/web/images/homeV2/rtc/KERALA-RTC.svg" />
+        <img src="https://st.redbus.in/buslogos/country/ind/logo_group/16426.png" />
+        <img src="https://s3.rdbuz.com/web/images/homeV2/rtc/RSRTC.svg" />
+        <img src='https://s3.rdbuz.com/web/images/homeV2/rtc/UPSRTC.svg'/>
+      </div>
+    </div>
   )
 }
 
