9f2c585a2a05f43b879b382a3115983120d41385
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-14 10:28:28 +0530

    #

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,16 @@ import '../styles/navbar.css'
 const Navbar = () => {
     return (
         <div className='Navbar'>
-            <div className="logo">
-                <h1>Hello<span>Travels</span></h1>
-            </div>
+            <Link to='/'>
+                <div className="logo">
+                    <img src="https://st.redbus.in/Images/rdc/rdc-redbus-logo.svg" />
+                    <h1 className='hello'>Hello<span>Travels</span></h1>
+                </div>
+            </Link>
             <div className="options">
-                <Link to='/aboutus'><InfoIcon id='mui1'/>About Us</Link>
-                <Link to='/contactus'><ContactSupportIcon id='mui2'/>Contact Us</Link>
-                <Link to='/userlogin'><AccountCircleIcon id='mui3'/>Login</Link>
+                <Link to='/aboutus'><InfoIcon id='mui1' />About Us</Link>
+                <Link to='/contactus'><ContactSupportIcon id='mui2' />Contact Us</Link>
+                <Link to='/userlogin'><AccountCircleIcon id='mui3' />Login</Link>
             </div>
         </div>
     )
