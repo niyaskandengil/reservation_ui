82affbc23a4a18512068eb78a2214c1f0e629e2f
Niyas Kandengil <52659398+niyaskandengil@users.noreply.github.com>
2023-09-14 10:29:11 +0530

    UserSignUp

--- src/components/UserSignUp.jsx
+++ src/components/UserSignUp.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const UserSignUp = () => {
+  const [name, setName] = useState("");
+  console.log(name)
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [date, setDate] = useState("");
+  const [aadhar, setAadhar] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <div>UserSignUp</div>
+    <div>
+      <form action="">
+        <input type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder='Enter Your Name' />
+        <br /><br />
+        <input type="email" value={email} onChange={(e) => { setEmail(e.target.value) }} placeholder='Enter Your Email' />
+        <br /><br />
+        <input type="tel" value={phone} onChange={(e) => { setPhone(e.target.value) }} placeholder='Enter Your Phone Number' />
+        <br /><br />
+        <input type="date" value={date} onChange={(e) => { setDate(e.target.value) }} />
+        <br /><br />
+        <input type="tel" value={aadhar} onChange={(e) => { setAadhar(e.target.value) }} placeholder='Enter Your Aadhar Number' />
+        <br /><br />
+        <input type="password" value={password} onChange={(e) => { setPassword(e.target.value) }} placeholder='Enter Your Password' />
+        <br /><br />
+      </form>
+    </div>
   )
 }
 
